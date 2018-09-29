3a7cf833d32edd1c0b6c7a3d380c79bd64d590f2
Arda Bilen <rdbilen@gmail.com>
2018-09-29 14:08:35 +0300

    Swagger integration

--- src/main/java/roadhint/AppConfig.java
+++ src/main/java/roadhint/AppConfig.java
@@ -1,11 +1,21 @@
 package roadhint;
 
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @Configuration
+@EnableSwagger2
 public class AppConfig {
     @Bean
     public JtsModule jtsModule() {
@@ -17,4 +27,16 @@ public class AppConfig {
     public GeometryFactory geometryFactory() {
         return new GeometryFactory();
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+
+
 }
